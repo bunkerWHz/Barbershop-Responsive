c05ee230c4a7c1726a069b3348baeac9f580fd94
aparsegov <aparsegov@gmail.com>
2017-04-08 14:42:37 +0300

    Add css sliders

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
 var mainNavLoginLink = mainNav.querySelector('.main-nav__user-login');
+var overlay = document.querySelector('.overlay');
 
 var loginForm = document.querySelector('.login-form');
 var loginFormBtn = document.querySelector('.login-form__btn-close');
+var loginFormSend = document.querySelector('.login-form__btn-login');
 
 mainNav.classList.remove('main-nav--nojs');
 
@@ -15,7 +17,7 @@ function toggleMainNav() {
 		mainNav.classList.add('main-nav--closed');
 		mainNav.classList.remove('main-nav--opened');
 	}
-};
+}
 
 function toggleLoginForm(e) {
 	e.preventDefault();
@@ -26,6 +28,37 @@ function toggleLoginForm(e) {
 	}
 }
 
+function toggleOverlay(e) {
+	e.preventDefault();
+	if (overlay.classList.contains('overlay--hidden')) {
+		overlay.classList.remove('overlay--hidden');
+		toggleLoginForm(e);
+	} else {
+		overlay.classList.add('overlay--hidden');
+		toggleLoginForm(e);
+	}
+}
+
+function validateForms(e) {
+	e.preventDefault();
+	if (!("FormData" in window)) {
+		return;
+	}
+
+	var data = new FormData(loginForm);
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("post", "/send?");
+	xhr.addEventListener("readystatechange", function(){
+		if (xhr.readyState == 4) {
+			console.log(xhr.responseText);
+		}
+	});
+		xhr.send(data);
+};
+
 mainNavToggle.addEventListener('click', toggleMainNav);
-mainNavLoginLink.addEventListener('click', toggleLoginForm);
-loginFormBtn.addEventListener('click', toggleLoginForm);
+mainNavLoginLink.addEventListener('click', toggleOverlay);
+loginFormBtn.addEventListener('click', toggleMainNav);
+loginFormSend.addEventListener('click', validateForms);
+overlay.addEventListener('click', toggleOverlay);
